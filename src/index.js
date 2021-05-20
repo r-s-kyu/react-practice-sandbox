3b2bb91cb43d94b824ac79bf08a81fffdcb11c70
r-s-kyu <83934713+r-s-kyu@users.noreply.github.com>
2021-05-20 17:04:48 +0900

    ファイルの土台作り

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
-import { StrictMode } from "react";
-import ReactDOM from "react-dom";
+import React from "react";
+import ReactDom from "react-dom";
 
-import App from "./App";
+const App = () => {
+  // returnの内容が複数ある際は（）で囲む必要あり
+  // JSX記法はreturnしていくhtml内容は1つのタグで囲わなけらばならない。
+  // そのため下記のように<React.Fragment>で囲うと、画面に何もレンダリングされない。
+  return (
+    // <React.Fragment>
+    //   <h1>こんにちは！</h1>
+    //   <p>お元気ですか？</p>
+    // </React.Fragment>
+    // これでもよい。
+    <>
+      <h1>こんにちは！</h1>
+      <p>お元気ですか？</p>
+    </>
+  );
+};
 
-const rootElement = document.getElementById("root");
-ReactDOM.render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-  rootElement
-);
+ReactDom.render(<App />, document.getElementById("root"));
